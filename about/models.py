f8e98bd89299e59e2034580ffd210056496172cd
joya <joyanyanwu14@gmail.com>
2020-06-20 17:09:51 +0100

    created banner image to go on all pages (expect homepage)

--- about/models.py
+++ about/models.py
@@ -1,18 +1,34 @@
 """About page."""
 from django.db import models
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class AboutPage(Page):
 
     template = "about/about_page.html"
 
-    subtitle = models.CharField(max_length=100, null=True, blank=True)
     # @todo add streamfields
-    #content = StreamField()
+    # content timezone= StreamField()
 
+    banner_title = models.CharField(max_length=20, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
+
+ 
     content_panels = Page.content_panels + [
-        FieldPanel("subtitle"),
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image"),
+    
     ]
 
 class Meta:
