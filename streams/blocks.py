e7e3100993b3a9fd8dcb1f0f7fb80b0812fa6a06
joya <joyanyanwu14@gmail.com>
2020-06-24 12:05:06 +0100

    started editing contact page

--- streams/blocks.py
+++ streams/blocks.py
@@ -6,10 +6,14 @@ from wagtail.admin.edit_handlers import (FieldPanel, FieldRowPanel,
                                          InlinePanel, MultiFieldPanel,
                                          PageChooserPanel, StreamFieldPanel)
 from wagtail.core import blocks
-from wagtail.core.fields import StreamField
+from wagtail.core.fields import StreamField, RichTextField
 from wagtail.embeds.blocks import EmbedBlock
 from wagtail.images.blocks import ImageChooserBlock
-
+from modelcluster.fields import ParentalKey
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField
+)
 
 class ColumnBlock(blocks.StreamBlock):
     heading = blocks.CharBlock(classname="full title")
@@ -83,3 +87,34 @@ class CTABlock(blocks.StructBlock):
         template = "streams/cta_block.html"
         icon = "placeholder"
         label = "Call to Action"
+"""
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'ContactPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
+
+class ContactPage(AbstractEmailForm):
+
+    template = "contact/contact_page.html"
+
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label='Form Fields'),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel("subject"),
+        ], heading="Email Settings"),
+        
+    ]
+
+    """
+    
