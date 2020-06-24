01b99dbfbae5fe9cd7af3ca0e2048711bde29969
joya <joyanyanwu14@gmail.com>
2020-06-24 10:55:26 +0100

    added cards, banner, richtext to donate page

--- donate/models.py
+++ donate/models.py
@@ -1,24 +1,56 @@
 from django.db import models
-
-"""Donate page."""
-from django.db import models
+from wagtail.core import blocks
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.embeds.blocks import EmbedBlock
+
+from streams import blocks
 
 class DonatePage(Page):
 
     template = "donate/donate_page.html"
 
-    subtitle = models.CharField(max_length=100, null=True, blank=True)
-    # @todo add streamfields
-    #content = StreamField()
+   
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
+
+
+
+    
+    body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel("subtitle"),
+        FieldPanel('body', classname="full"),
+        FieldPanel('banner_title'),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel('banner_image'), 
+        StreamFieldPanel("content"),
     ]
 
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("cards", blocks.CardBlock()),
+            ("cta", blocks.CTABlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+   
+   
 class Meta:
         verbose_name = "Donate Page"
         verbose_name_plural = "Donate Pages"
 
 # Create your models here.
+
